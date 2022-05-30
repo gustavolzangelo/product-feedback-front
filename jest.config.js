21379d0fae5efb9f3bd3e17ee5ffbc54926a8895
Gustavo Angelo <gustavo.angelo@elogroup.com.br>
2022-05-30 19:38:04 -0300

    feat: docker configuration

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,28 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  coveragePathIgnorePatterns: ['<rootDir>/pages/_app.tsx'],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
+  },
   testEnvironment: 'jest-environment-jsdom',
 }
 
