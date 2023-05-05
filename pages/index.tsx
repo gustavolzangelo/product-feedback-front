fc5f148e4ced073a8a8fcc84c04f517ff3acf119
gustavolzangelo <gustavolz.angelo@gmail.com>
2023-05-05 17:26:24 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+console.log("oi");
   return (
     <div className={styles.container}>
       <Head>
